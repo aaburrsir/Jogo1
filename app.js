3b1b7156d3952fdcbec67ac069ccff6212326e1a
Micaella <micaella.cavalcante13@gmail.com>
2025-06-27 16:45:43 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,104 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+// function verificarChute() {
+//   console.log('O botão foi clicado!');
+// }
+let numMaximo = 100;
+let listaNumerosGerados = [];
+let numeroSecreto = gerarNumAleatorio();
+let tentativas = 1;
+//reiniciarJogo()
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function ExibirMsgInicial(){
+    exibirTextoNaTela('h1','Jogo do número secreto');
+    exibirTextoNaTela('p',`Escolha um número entre 1 e ${numMaximo}`);
+}
+
+ExibirMsgInicial();
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        limparCampo()
+        let palavra = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavra}!`
+        exibirTextoNaTela('p', mensagemTentativas);
+         document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor!');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas ++;
+        limparCampo()
+    }
+}
+
+function gerarNumAleatorio() {
+    let numGerado = parseInt(Math.random() * numMaximo + 1);
+    let qtdNumLista = listaNumerosGerados.length;
+
+    if (qtdNumLista == numMaximo) {
+        listaNumerosGerados = [];
+    }
+    if (listaNumerosGerados.includes(numGerado)) {
+        return gerarNumAleatorio();
+    } else {
+        listaNumerosGerados.push(numGerado);
+        console.log('O numero secreto é', numGerado);
+        console.log('Numeros já sorteados: ', listaNumerosGerados);
+        return numGerado;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumAleatorio();
+    tentativas = 1;
+    limparCampo();
+    ExibirMsgInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+}
+
+
+
+
+////////////////// atividades /////////////////////
+
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Hora do Desafio';
+
+// function exibirMensagemNoConsole() {
+//     console.log('O botão foi clicado!')
+// }
+
+// function exibirAlerta(){
+//     alert('Eu amo Js');
+// }
+
+// function exibirPrompt(){
+//     let cidade = prompt('Digite o nome da cidade')
+//     alert(`Estive em ${cidade} e lembrei de você!`)
+// }
+
+// function somandoDoisNumeros(){
+//     let num1 = parseInt(prompt('Digite o primeiro número'));
+//     let num2 = parseInt(prompt('Digite o segundo número'));
+//     alert(`${num1} + ${num2} = ${num1 + num2}`);
+// }
---
+++ atividade1.js
@@ -0,0 +1,31 @@
+// Criar uma função que exibe "Olá, mundo!" no console.
+function ExibeTexto(texto){
+    console.log(texto);
+}
+
+ExibeTexto('Olá, mundo!')
+
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+// let uai = prompt('Digite seu nome')
+// ExibeNome(uai)
+
+function ExibeNome(uai) {
+    console.log(`Olá, ${uai}!`);
+}
+
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+let num = 5
+
+function DobroNum(num){
+    //num = num * 2;
+    return num *2;
+}
+
+dobro = DobroNum(num);
+console.log(dobro);
+
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo

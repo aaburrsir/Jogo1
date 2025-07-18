7de4dd3b2608995d9eb83018acd3162e50f93ebd
Micaella <micaella.cavalcante13@gmail.com>
2025-07-18 17:41:08 -0300

    teste 2

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do número secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
-// function verificarChute() {
-//   console.log('O botão foi clicado!');
-// }
 let numMaximo = 100;
 let listaNumerosGerados = [];
 let numeroSecreto = gerarNumAleatorio();
 let tentativas = 1;
-//reiniciarJogo()
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
